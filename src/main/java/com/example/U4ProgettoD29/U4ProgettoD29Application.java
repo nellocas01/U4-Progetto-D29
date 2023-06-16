e79b9f42a370e2407d0d3b494c194fe907d66049
Nello Casolla <nellocasolla446@gmail.com>
2023-06-16 10:34:36 +0200

    assets

---
+++ src/main/java/com/example/U4ProgettoD29/U4ProgettoD29Application.java
@@ -0,0 +1,13 @@
+package com.example.U4ProgettoD29;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U4ProgettoD29Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U4ProgettoD29Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/U4ProgettoD29/U4ProgettoD29ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.U4ProgettoD29;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U4ProgettoD29ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
